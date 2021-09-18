1354bd54b329f5d9cb80b1f684135f9f6ddf892a
3433825 <80127761+3433825@users.noreply.github.com>
2021-09-18 09:35:50 +0300

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,15 +1,11 @@
 ﻿def calc(input):
     return eval(input)
-
-
 print(calc('5+2'))
 print(calc(' ( 5 + 2 ) * 2'))
 print(calc(' 5 + 2 * 2'))
 
 # второй вариант
 
-code1 = compile("5 + 2", "<string>", "eval")
-code2 = compile("( 5 + 2 ) * 2", "<string>", "eval")
-code3 = compile("5 + 2 * 2", "<string>", "eval")
-
-print(eval(code1))
+print(eval(compile("5 + 2", "<string>", "eval")))
+print(eval(compile("( 5 + 2 ) * 2", "<string>", "eval")))
+print(eval(compile("5 + 2 * 2", "<string>", "eval")))
