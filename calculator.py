5a35bfa475f425bfbe5685816f56e252048ef5f3
O.Shapovalov <shapovalov.olexiy@gmail.com>
2021-09-18 09:58:14 +0300

    remove unnecessary code

--- calculator.py
+++ calculator.py
@@ -3,9 +3,3 @@
 print(calc('5+2'))
 print(calc(' ( 5 + 2 ) * 2'))
 print(calc(' 5 + 2 * 2'))
-
-# второй вариант
-
-print(eval(compile("5 + 2", "<string>", "eval")))
-print(eval(compile("( 5 + 2 ) * 2", "<string>", "eval")))
-print(eval(compile("5 + 2 * 2", "<string>", "eval")))
