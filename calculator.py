90c587b7f182c5927be098744528991518278aa7
3433825 <80127761+3433825@users.noreply.github.com>
2021-09-18 11:10:45 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 ﻿def calc(input):
     return eval(input)
-print(calc('5+2'))
-print(calc(' ( 5 + 2 ) * 2'))
-print(calc(' 5 + 2 * 2'))
+print(calc('5 + 2'))
+print(calc('( 5 + 2 ) * 2'))
+print(calc('5 + 2 * 2'))
