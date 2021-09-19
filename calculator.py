5861ee5eb3ea92326b5e930d5d2363db93224c4d
3433825 <80127761+3433825@users.noreply.github.com>
2021-09-19 20:35:28 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 ﻿def calc(input):
     return eval(input)
-inputs = ('5 + 2', '( 5 + 2 ) * 2', '5 + 2 * 2')
+inputs = ['5 + 2', '( 5 + 2 ) * 2', '5 + 2 * 2']
 for input in inputs:
     print(f'{input} = {calc(input)}')
