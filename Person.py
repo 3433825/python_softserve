1d1b975dbad365de5a596ee881cac0799d1907b7
Oleksii Shapovalov <shapovalov.olexiy@gmail.com>
2021-09-23 15:24:48 +0300

    Implement Person, Student, Teacher classes

--- Person.py
+++ Person.py
@@ -1,4 +1,3 @@
-# Примитивное описание человека
 class Person():
     def __init__(self, age, height):
         self.age = age
@@ -6,7 +5,33 @@ class Person():
 
     def get_weight(self):
         weight = self.height - 100
-        print(f'The person weight is {weight} kg')
+        return weight
 
-student_person = Person(25, 184)
-student_person.get_weight()
+class Student(Person):
+    """ docs """
+    def __init__(self, age, height, group):
+        super().__init__(age, height)
+        self.group = group
+
+    def get_weight(self):
+        person_weight = super().get_weight()
+        student_weight = person_weight - 10
+        return student_weight
+
+class Teacher(Person):
+    def __init__(self, age, height, experience):
+        super().__init__(age, height)
+        self.experience = experience
+
+    def get_weight(self):
+        person_weight = super().get_weight()
+        teacher_weight = person_weight + 10
+        return teacher_weight
+
+person = Person(35, 186)
+student = Student(20, 185, "A")
+teacher = Teacher(42, 178, 10)
+
+print(f'The person weight is {person.get_weight()} kg')
+print(f'The student weight is {student.get_weight()} kg')
+print(f'The teacher weight is {teacher.get_weight()} kg')
