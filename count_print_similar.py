a2a0e61d7110456cd718f6972e15e2101ac1bda8
Oleksii Shapovalov <shapovalov.olexiy@gmail.com>
2023-08-04 11:16:08 +0300

    added count_print_similar.py

---
+++ count_print_similar.py
@@ -0,0 +1,24 @@
+from string import punctuation
+from Data import data
+
+def find_simil_words(text: str):
+    """Make massive similar words"""
+    # Making list of words
+    words = text.lower().split()
+    words = list(map(lambda word: word.strip(punctuation), words))
+
+    #Making dict similar words
+    word_counts = {}
+
+    # Перебираем каждое слово в предложении
+    for word in words:
+        # Используем метод get() словаря для увеличения счетчика вхождений слова
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    # Формируем список повторяющихся слов
+    duplicates = [(word, count) for word, count in word_counts.items() if count > 1]
+
+    return duplicates
+
+
+print(find_simil_words(data.SENTENCE))
