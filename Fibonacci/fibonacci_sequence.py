05cf57fb761e69a842f9fdbbb661736b150682b3
Oleksii Shapovalov <shapovalov.olexiy@gmail.com>
2023-08-21 12:21:15 +0300

    corrected docstring

--- Fibonacci/fibonacci_sequence.py
+++ Fibonacci/fibonacci_sequence.py
@@ -1,9 +1,12 @@
 def get_element_fibonacci_sequence(n: int) -> int:
     """
     :param n:
-    :return: n-element og Fibonacci sequence.
+    :return: n-element of Fibonacci sequence.
     """
 
     if not isinstance(n, int):
         raise TypeError(f"argument of fib() must be integers, not {type(n)}")
     return n if n < 2 else get_element_fibonacci_sequence(n - 1) + get_element_fibonacci_sequence(n - 2)
+
+
+print(get_element_fibonacci_sequence(6))
