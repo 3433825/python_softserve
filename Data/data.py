0e761e5e433958098f484673ed4bdcfc009bfd8c
Oleksii Shapovalov <shapovalov.olexiy@gmail.com>
2023-08-21 10:56:12 +0300

    changed structure added test fibonacci sequence

--- Data/data.py
+++ Data/data.py
@@ -2,3 +2,21 @@ SENTENCE = ("Солнце сияло ярко, его свет порождал 
             "поддерживали жизнь обитателей океана. Свет солнца проникал глубоко.")
 SUBTITLE = ("We then added more debugging information to the scripts, which helped us find the root cause of the "
             "problem, a mismatch between the date formats used by this Zenity invocation and the Python script")
+ELEMENT_FIBONACCI = {
+    "argnames": ("index", "value"),
+    "argvalues": [
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 2),
+        (4, 3),
+        (5, 5)],
+    "ids": [
+        "1 element",
+        "2 element",
+        "3 element",
+        "4 element",
+        "5 element",
+        "6 element",
+    ]
+}
---
+++ Fibonacci/fibonacci_itvdn.py
@@ -0,0 +1,22 @@
+def fib(n: int) -> int:
+    """
+    :param n:
+    :return: n-element og Fibonacci sequence.
+    """
+
+    if not isinstance(n, int):
+        raise TypeError(f"argument of fib() must be integers, not {type(n)}")
+    return n if n < 2 else fib(n - 1) + fib(n - 2)
+
+
+if __name__ == "__main__":
+    test_cases = [
+        (0, 0),
+        (1, 1),
+        (2, 1),
+        (3, 2),
+        (4, 3)
+    ]
+    for arg, result in test_cases:
+        # print(fib(arg), f"{fib(arg) == result}")
+        assert fib(arg) == result, f"ERROR! fib({arg}) != {result}"
