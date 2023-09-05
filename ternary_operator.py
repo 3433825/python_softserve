86880535509c08db2471b07ccb9a5424bc505c61
Oleksii Shapovalov <shapovalov.olexiy@gmail.com>
2023-09-05 20:48:38 +0300

    added description ternary operator

--- ternary_operator.py
+++ ternary_operator.py
@@ -18,18 +18,19 @@ a, b = 10, 20
 
 # Copy value of a in min if a < b else copy b
 less_number = a if a < b else b
+greater_number = a if a > b else b
 
-print(less_number)
+print(f"From numbers {a}, {b} less_number {less_number}")
+print(f"From numbers {a}, {b} greater_number {greater_number}")
 # *********************************************************
 """
 In this example, we are using a nested if-else to demonstrate ternary operator. If a and b are equal then we will
 print a and b are equal and else if a>b then we will print a is greater than b otherwise b is greater than a.
 """
 
-a, b = 10, 20
-
-print ("Both a and b are equal" if a == b else "a is greater than b"
-        if a > b else "b is greater than a")
+comparison_result = "Both a and b are equal" if a == b else "a is greater than b" if a > b else "b is greater than a"
+print(f"Comparison result {a} and {b} is {comparison_result}")
+print()
 
 # **********************************************************
 
@@ -39,6 +40,31 @@ if [a < b] is true it return 1, so element with 1 index will print else if [a<b]
 0 index will print.
 """
 # Python program to demonstrate ternary operator
-a, b = 10, 20
 
-print( (b, a) [a < b] )
+less_number = (b, a)[a < b]  # if true element[1] is returned
+print(f"Ternary operator like this 'less_number = (b, a) [a < b]' returns less_number = {less_number}")
+
+greater_number = (a, b)[a < b]  # if true element[1] is returned
+print(f"Ternary operator like this 'greater_number = (a, b) [a < b]' returns greater_number = {greater_number}")
+print()
+# ***********************************************************************
+
+"""
+In this example, we are using Dictionary to demonstrate ternary operator and
+if [a<b] is true it return value of True key, so value of True key will print else if [a<b] is false value of False key 
+will print.
+"""
+
+less_number = {True: a, False: b} [a < b]
+print("Ternary operator like this 'less_number = {True: a, False: b} [a < b]'" + " " +
+      f"returns less_number = {less_number}")
+greater_number = {True: a, False: b} [a > b]
+print("Ternary operator like this 'greater_number = {True: a, False: b} [a > b]'" + " " +
+      f"returns greater_number = {greater_number}")
+print()
+
+# ******************************************************************
+
+print((lambda: b, lambda: a)[a < b]())  # if true element[1] is returned
+
+print((lambda: b, lambda: a)[a > b]())  # if false element[0] is returned
