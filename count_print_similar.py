a05dbcbae009668abc5b83d25c3474ced3bf9b3b
Oleksii Shapovalov <shapovalov.olexiy@gmail.com>
2023-10-18 11:45:29 +0300

    new solution applied

--- count_print_similar.py
+++ count_print_similar.py
@@ -32,3 +32,56 @@ for t in duplicates_words_count:
     print(t)
 
 print(find_simil_words(data.SENTENCE))
+
+# *************************************************************************************************
+
+"""
+from collections import Counter
+
+def sort_least_repeated(array):
+    counter = Counter(array)  # Подсчет числа вхождений каждого элемента
+    least_repeated = sorted(counter.items(), key=lambda x: (x[1], -x[0]))  # Сортировка по наименее встречаемым
+    элементам
+
+    sorted_array = []
+    for element, count in least_repeated:
+        sorted_array.extend([element] * count)  # Добавление элементов в результирующий массив в соответствии с их
+                                                # количеством
+
+    return sorted_array
+Вызов функции sort_least_repeated с массивом целых чисел k вернет отсортированный массив с наименее повторяющимися
+числами. Если несколько чисел имеют одинаковое количество повторений, они будут отсортированы в порядке убывания.
+
+"""
+
+from collections import Counter
+sentence_inst = data.SENTENCE
+
+
+def find_duplicated_words(sentence):
+    """Make massive similar words"""
+    # Making list of words
+    words = sentence.lower().split()
+    words = list(map(lambda word: word.strip(punctuation), words))
+
+    #Making dict similar words
+    word_counts = {}
+    counter = Counter(words)  # Подсчет числа вхождений каждого элемента
+    print()
+    print(counter)
+
+    duplicated_words = []
+    duplicated_words_dict = {}
+    for element, count in counter.items():
+        if count > 1:
+            duplicated_words.append(element)  # Добавление элементов в результирующий массив в соответствии с их
+                                              # количеством
+            duplicated_words_dict[element] = count
+
+    return duplicated_words, duplicated_words_dict
+
+
+
+
+print(find_duplicated_words(sentence_inst)[0])
+print(find_duplicated_words(sentence_inst)[1])
