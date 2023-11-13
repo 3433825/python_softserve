3a96328b805098fdd6aa35ca0aede09692bb0657
3433825 <shapovalov.olexiy@gmail.com>
2023-11-13 12:07:29 +0200

    added merge sort

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -22,3 +22,34 @@ def merge_sort(arr):
     # perform merge_sort recursively on both sides
     left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
 
+    print(left)
+    print(right)
+
+    # merge the sorted halves
+    return merge(left, right, arr.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        # sort each one and put it into the result
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor+right_cursor]=left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+arr_l = data.UNSORTED_NUMBERS_LIST
+print(arr_l)
+print(merge_sort(arr_l))
+
