6005df80d7db29a75be7b3695a115103564586b9
3433825 <shapovalov.olexiy@gmail.com>
2023-12-07 18:34:14 +0200

    Run time measurement added

--- count_similar/massive_simil_func.py
+++ count_similar/massive_simil_func.py
@@ -1,3 +1,7 @@
+import timeit
+from Data import data
+from Fibonacci.duration import duration
+
 def simil_words_ru(text):
     """Make massive similar words"""
 
@@ -10,6 +14,7 @@ def simil_words_ru(text):
         word_without_punctuation = word.strip(punctuation)
         if word_without_punctuation:
             words.append(word_without_punctuation)
+
 
 #Making list of similar words
     numbers = list(range(0, len(words)))
@@ -24,3 +29,15 @@ def simil_words_ru(text):
                 if number != number2:
                     similar_words[number].append(words[number2])
     return similar_words
+
+
+text = data.SENTENCE
+execution_time = timeit.timeit(lambda: simil_words_ru(text), number=1)
+print(f"execution_time with timeit module: {execution_time:.6f} s")
+print()
+
+
+@duration
+def count_similar_words_ru(text):
+    return simil_words_ru(text)
+print(count_similar_words_ru(text))
